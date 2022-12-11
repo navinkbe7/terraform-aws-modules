430de4a02d8aaa0aa4fca4a121d1c44790fe44c2
navinkbe7 <navinkbe7@gmail.com>
2022-12-10 18:43:40 -0600

    module aws-s3-static-website-bucket

---
+++ aws-tf-s3/main.tf
@@ -0,0 +1,12 @@
+module "website_s3_bucket" {
+  source = "./modules/aws-s3-static-website-bucket"
+
+  bucket_name = "animals-for-life-nilango"
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
+
+
---
+++ aws-tf-s3/modules/aws-s3-static-website-bucket/main.tf
@@ -0,0 +1,43 @@
+resource "aws_s3_bucket" "s3_bucket" {
+  bucket = var.bucket_name
+
+  tags = var.tags
+}
+
+resource "aws_s3_bucket_website_configuration" "s3_bucket" {
+  bucket = aws_s3_bucket.s3_bucket.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
+}
+
+#TODO: By default the bucket endpoint is HTTP and need to enable HTTPS
+# https://adamtheautomator.com/aws-s3-static-ssl-website/
+
+resource "aws_s3_bucket_policy" "public_read" {
+  bucket = aws_s3_bucket.s3_bucket.id
+  policy = data.aws_iam_policy_document.public_read.json
+}
+
+data "aws_iam_policy_document" "public_read" {
+  statement {
+    sid = "PublicRead"
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+
+    actions = [
+      "s3:GetObject"
+    ]
+
+    resources = [
+      "${aws_s3_bucket.s3_bucket.arn}/*",
+    ]
+  }
+}
